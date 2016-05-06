c1fe40b27ae2afc49e669152bf19b99c9f96c47a
Graphic <graphic@SOD-SJH-516-03M.local>
2016-05-06 19:02:06 -0400

    Added leg movement

---
+++ Assets/Scripts/Leg.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum LegType { Lower, Upper }
+
+public class Leg : MonoBehaviour {
+
+	public GameObject lowerLeg { get; private set; }
+	public GameObject upperLeg { get; private set; }
+
+	HingeJoint2D lowerHinge, upperHinge;
+
+	void Awake () {
+		lowerLeg = transform.Find ("Lower").gameObject;
+		upperLeg = transform.Find ("Upper").gameObject;
+		lowerHinge = lowerLeg.GetComponent<HingeJoint2D> ();
+		upperHinge = upperLeg.GetComponent<HingeJoint2D> ();
+		lowerHinge.useMotor = upperHinge.useMotor = true;
+	}
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void SetMotorSpeed (LegType legType, float speed) {
+		JointMotor2D motor;
+		switch (legType) {
+		case LegType.Lower:
+			if (lowerHinge == null) throw new UnityException ("Lower Hinge is null");
+			motor = lowerHinge.motor;
+			motor.motorSpeed = speed;
+			lowerHinge.motor = motor;
+			break;
+		case LegType.Upper:
+			if (upperHinge == null) throw new UnityException ("Upper Hinge is null");
+			motor = upperHinge.motor;
+			motor.motorSpeed = speed;
+			upperHinge.motor = motor;
+			break;
+		}
+	}
+
+	public float GetMotorSpeed (LegType legType) {
+		float motorSpeed = 0;
+		switch (legType) {
+		case LegType.Lower:
+			if (lowerHinge == null) throw new UnityException ("Lower Hinge is null");
+			motorSpeed = lowerHinge.motor.motorSpeed;
+			break;
+		case LegType.Upper:
+			if (upperHinge == null) throw new UnityException ("Upper Hinge is null");
+			motorSpeed = upperHinge.motor.motorSpeed;
+			break;
+		}
+		return motorSpeed;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerType { Front, Back }
+
+public class Player : MonoBehaviour {
+
+	public PlayerType playerType = PlayerType.Front;
+	public float legSpeed = 1.0f;
+
+	Leg leftLeg, rightLeg;
+
+	void Awake () {
+		//leftLeg = transform.Find ("Left Leg").GetComponent<Leg>();
+		rightLeg = transform.Find ("Right Leg").GetComponent<Leg>();
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		rightLeg.SetMotorSpeed (LegType.Lower, Input.GetAxis ("Horizontal") * legSpeed);
+		rightLeg.SetMotorSpeed (LegType.Upper, Input.GetAxis ("Vertical") * legSpeed);
+	}
+}
